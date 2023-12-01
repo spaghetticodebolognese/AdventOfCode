1f8d4a1caec4a8f18f08cf43cbeef10049b63626
spaghetticodebolognese <darnold22769@gmx.de>
2023-12-01 18:12:30 +0100

    day 01 part 1 complete

--- Advent of Code/src/Day01.java
+++ Advent of Code/src/Day01.java
@@ -1,11 +1,41 @@
-import java.util.ArrayList;
-import java.util.regex.*;
-
-public class Main {
+public class Day01 {
     public static void main(String[] args) {
 
+        /*
+        --- Day 1: Trebuchet?! ---
+
+Something is wrong with global snow production, and you've been selected to take a look.
+The Elves have even given you a map; on it, they've used stars to mark the top fifty locations that are likely to be having problems.
+You've been doing this long enough to know that to restore snow operations,
+you need to check all fifty stars by December 25th.
+Collect stars by solving puzzles. Two puzzles will be made available on each day in the Advent calendar;
+the second puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
+You try to ask why they can't just use a weather machine ("not powerful enough") and where they're even
+sending you ("the sky") and why your map looks mostly blank ("you sure ask a lot of questions") and hang
+on did you just say the sky ("of course, where do you think snow comes from") when you realize that the
+Elves are already loading you into a trebuchet ("please hold still, we need to strap you in").
+As they're making the final adjustments, they discover that their calibration document (your puzzle input)
+has been amended by a very young Elf who was apparently just excited to show off her art skills.
+Consequently, the Elves are having trouble reading the values on the document.
+The newly-improved calibration document consists of lines of text; each line originally contained a
+specific calibration value that the Elves now need to recover.
+On each line, the calibration value can be found by combining the first digit and the last digit (in that order) to form a single two-digit number.
+
+For example:
+
+1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet
+
+In this example, the calibration values of these four lines are 12, 38, 15, and 77. Adding these together produces 142.
+Consider your entire calibration document. What is the sum of all of the calibration values?
+
+         */
+
+
         String input = "eighttkbtzjz6nineeight\n" +
-                "5knjbxgvhktvfcq89onefive\n" +
+                "5knjbxgvhktvfcq89onefive\n"  +
                 "hnjcrxeightonejnlvm4hstmcsevensix\n" +
                 "trsdgcxcseven39dpmzs\n" +
                 "oneninesixtwo26\n" +
@@ -1004,13 +1034,14 @@ public class Main {
                 "blhsm4xcrbrf68ninezvhhtqgphnzxlhl\n" +
                 "9dvjvfourtcthree\n" +
                 "onethreenfkgrvsevenkczctlgkt7";
-        //System.out.println(trebuchetCalibrationValue(input));
 
-            String[] splittedInput = input.split("\n");
 
+        String[] splittedInput = input.split("\n");
+        int sum = 0;
         for(String eintrag : splittedInput){
-            System.out.println(trebuchetCalibrationValue(eintrag));
+            sum += Integer.parseInt(trebuchetCalibrationValue(eintrag));
         }
+            System.out.println(sum);
 
 
 
@@ -1024,15 +1055,18 @@ public class Main {
         for (int i = 0; i < charArray.length; i++) {
             if (Character.isDigit(charArray[i]) && firstNum == -1) {
                 firstNum = charArray[i];
-                //System.out.println("First Num: " + charArray[i]);
             }
         }
-        for (int j = charArray.length-1; j > 0; j--) {
-            if (Character.isDigit(charArray[j]) && secondNum == -1) {
-                secondNum = charArray[j];
-                //System.out.println("Second Num: " + charArray[j]);
+        for (int i = charArray.length-1; i > 0; i--) {
+            if (Character.isDigit(charArray[i]) && secondNum == -1) {
+                secondNum = charArray[i];
             }
         }
+
+        if(secondNum == -1){
+            secondNum = firstNum;
+        }
+
         String result1 = Integer.toString(Character.getNumericValue(firstNum));
         String result2 = Integer.toString(Character.getNumericValue(secondNum));
         return result1+result2;
